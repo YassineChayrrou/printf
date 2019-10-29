409fcf42a8c48380e8fc6a123829101952febe62
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-29 23:32:03 +0100

    commits

--- holberton.h
+++ holberton.h
@@ -1,4 +1,23 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdarg.h>
+/**
+ * struct print - defines type print_t
+ * @print: pointer to char
+ * @p: function pointer
+ *
+ */
+typedef struct print
+{
+	char *print;
+	int (*p)();
+} print_t;
 
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+int p_char(va_list arg);
+int p_str(va_list arg);
+int p_dec(va_list arg);
+int p_int(va_list arg);
 #endif /* HOLBERTON_H */
