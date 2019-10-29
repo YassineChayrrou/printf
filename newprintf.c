c81ce2ec9f72a85d9100fdc763c2ab1794d58176
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-10-30 00:01:51 +0100

    fix

--- newprintf.c
+++ newprintf.c
@@ -2,11 +2,12 @@
 #include <stdarg.h>
 #include "holberton.h"
 /**
- * _printf - prints an output according to a format specifier
+ * _printf - produces output according to a format
  * @format: character string
  * Return: number of characters printed excluding null byte
  */
 int _printf(const char *format, ...)
+{
 	va_list arg;
 	unsigned int i, j, flag;
 	unsigned int len = 0;
@@ -15,10 +16,12 @@ int _printf(const char *format, ...)
 		{"c", p_char}, {"s", p_str}, {"d", p_dec}, {"i", p_int},
 		{NULL, NULL}
 	};
+
 	va_start(arg, format);
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (0);
+
 	i = 0;
 	while (format[i] != '\0')
 	{
