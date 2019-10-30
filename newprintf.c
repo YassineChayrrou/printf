5718e7b1a4ae55b2d76ebd90012c8f242f36db75
koussay-dellai <koussaydellai22@gmail.com>
2019-10-30 03:10:28 +0100

    printf function

--- newprintf.c
+++ newprintf.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-#include <stdarg.h>
 #include "holberton.h"
-
 /**
  * _printf - replicant of printf of stdio.h
  * @format: character string
@@ -12,17 +10,13 @@ int _printf(const char *format, ...)
 	va_list arg;
 	unsigned int i, j, flag;
 	unsigned int len = 0;
-
 	print_t print[] = {
 		{"c", p_char}, {"s", p_str}, {"d", p_dec}, {"i", p_int},
 		{NULL, NULL}
 	};
-
 	va_start(arg, format);
-
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (0);
-
 	i = 0;
 	while (format[i] != '\0')
 	{
@@ -60,6 +54,5 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(arg);
-
 	return (len);
 }
